d94259eaec6fd52a83196b3be8dfa3b02dc0812f
Fadzli Fadzil <fadzli.mdfadzil@singtel.com>
2019-06-26 15:40:38 +0800

    sig

--- model/S3.php
+++ model/S3.php
@@ -42,7 +42,6 @@ class S3 {
 			// $resource = fopen('/path/to/file', 'r');
 			$s3 = static::singleton();
 			$s3client = S3Client::factory([
-				'region' => 'ap-southeast-1',
 				'signature' => 'v4'
 			]);
 			$resource = EntityBody::factory($data);
